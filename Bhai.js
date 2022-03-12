d7692d797f7972140db91804572ad8fc3ce99cc9
Yash Dayal <yash.dayal@gmail.com>
2022-03-13 00:24:32 +0530

    Create Bhai.js

---
+++ Bhai.js
@@ -0,0 +1,19 @@
+hi bhai
+ bol bhai "Welcome to Mum-bhai Underworld";
+ 
+  bhai ye hai dawood_bhai = 3;
+  bhai ye hai chota_shakeel = 0;
+
+  jab tak bhai (chota_shakeel < dawood_bhai) {
+    bol bhai "Dawood hai Mum-bhai ka don!";
+
+
+
+    chota_shakeel += 1;
+  }
+
+  agar bhai (chota_shakeel == dawood_bhai){
+    bol bhai "Dawood ka singhasan dole raha bhai";
+  }
+
+bye bhai
